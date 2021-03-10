82d8ff4646faac46004c199d33c2c6c5b30faf19
kocierik <kocierik@gmail.com>
2021-03-10 18:41:21 +0100

    feat: add react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="list-menu"></div>
+      <div className="container-content"></div>
+      <div className="info-menu"></div>
+    </div>
+  );
+}
+
+export default App;
