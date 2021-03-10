2b26c43e830201e7f5f5db112f55733c6ecb9379
kocierik <kocierik@gmail.com>
2021-03-11 00:20:15 +0100

    feat: add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-
+import "./image.jpg"
 function App() {
   return (
     <div className="container">
-      <div className="list-menu"></div>
-      <div className="container-content"></div>
-      <div className="info-menu"></div>
+      <div className="header">
+      <div className="header-title">AskMe</div>
+      </div>
+      <div className="container-content">
+        <div className="container-content_left"></div>  
+        <div className="container-content_right">
+          <div className="container-form">
+            <div className="title-login"> Sign in</div>
+            <div className="line"></div>
+            <form>
+              <label>username</label>
+                <input type="text" size="30"></input>
+              <label>password</label>
+                <input type="text" size="30"></input>
+              <input type="submit" value="submit" className="submit-button"></input>
+            </form>
+          </div>
+        </div>
+
+      </div>
+      <div className="footer">
+        <div className="line"></div>
+      </div>
     </div>
   );
 }
